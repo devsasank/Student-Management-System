2f59963af6474287bb002add93db220513bc57e9
Sasank Subudhi <sasanksubudhi63@gmail.com>
2026-06-07 18:23:27 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,21 @@
+public class Student {
+    int studentId;
+    String name;
+    int age;
+    String course;
 
+    public Student(int studentId, String name, int age, String course) {
+        this.studentId = studentId;
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+
+    public void displayStudent() {
+        System.out.println("ID: " + studentId);
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+        System.out.println("-----------------------");
+    }
+}
